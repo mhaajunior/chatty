7812d47cab54fc236940842d065bc9f99a22d035
Thithut <junior29958@gmail.com>
2025-03-18 15:54:18 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // กำหนดให้ไฟล์ถูกสร้างในโฟลเดอร์ `dist`
-  },
 });
